c080619a1315ee92a26aa69224800c9e75071501
clonaz <viragtamas187@gmail.com>
2024-08-31 22:17:48 +0200

    ccsc

--- app/components/subscribe.js
+++ app/components/subscribe.js
@@ -1,20 +1,35 @@
-// components/subscribe.js
 import { loadStripe } from '@stripe/stripe-js';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
 export default function CheckoutButton() {
-  const handleClick = async () => {
+  const handleSubscribe = async () => {
     const stripe = await stripePromise;
+//s
+    if (!stripe) {
+      console.error('Stripe.js has not loaded');
+      return;
+    }
 
     const response = await fetch('/api/checkout', { method: 'POST' });
+
+    if (!response.ok) {
+      console.error('Failed to create checkout session');
+      return;
+    }
+
     const { sessionId } = await response.json();
 
     const { error } = await stripe.redirectToCheckout({ sessionId });
+
     if (error) {
       console.error('Error redirecting to checkout:', error);
     }
   };
 
-  return <button onClick={handleClick}>Subscribe</button>;
+  return (
+    <button role="link" onClick={handleSubscribe}>
+      Subscribe
+    </button>
+  );
 }
