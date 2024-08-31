eaeeda00b9d664d80324b2e2a9d47aaaed62b918
clonaz <viragtamas187@gmail.com>
2024-08-31 20:57:22 +0200

    browserless fixed

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,11 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-<<<<<<< HEAD
 // Your web app's Firebase configuration using environment variables
-=======
-// Your web app's Firebase configuration from environment variables
->>>>>>> dd014170031abab1302d77145d71257c38704bd4
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -13,11 +9,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-<<<<<<< HEAD
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-=======
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
->>>>>>> dd014170031abab1302d77145d71257c38704bd4
 };
 
 // Initialize Firebase
